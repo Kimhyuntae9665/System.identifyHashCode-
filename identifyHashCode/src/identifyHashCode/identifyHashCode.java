604774f07649db6f0ea81f08c32a9666a153af69
rlagusxo <rlagusxo@DESKTOP-M6P2TK9>
2022-12-29 16:28:23 +0900

    주소까지

--- identifyHashCode/src/identifyHashCode/identifyHashCode.java
+++ identifyHashCode/src/identifyHashCode/identifyHashCode.java
@@ -6,13 +6,13 @@ public class identifyHashCode {
 		String str1 = new String("kim"); //Heap 에 서로 다른 메모리에 저장 
 		String str2 = new String("kim");
 		
-		System.out.println(System.identityHashCode(str1));
-		System.out.println(System.identityHashCode(str2));
+		System.out.println(System.identityHashCode(str1));//1586600255
+		System.out.println(System.identityHashCode(str2));//474675244
 		
 		String str3 = "kimmm";
 		String str4 = "kimmm";
-		System.out.println(System.identityHashCode(str3));
-		System.out.println(System.identityHashCode(str4));
+		System.out.println(System.identityHashCode(str3));//932583850
+		System.out.println(System.identityHashCode(str4));//932583850
 
 	}
 
