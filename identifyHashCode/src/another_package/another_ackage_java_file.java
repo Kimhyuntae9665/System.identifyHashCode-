23758d6fa7aa82c2216a39cac72d35e2d148edf0
rlagusxo <rlagusxo@DESKTOP-M6P2TK9>
2022-12-29 16:39:06 +0900

    hide untracked file

---
+++ identifyHashCode/src/another_package/another_ackage_java_file.java
@@ -0,0 +1,9 @@
+package another_package;
+
+public class another_ackage_java_file {
+
+	public static void main(String[] args) {
+		System.out.println("what");
+	}
+
+}
---
+++ identifyHashCode/src/identifyHashCode/New_package_at_the_package.java
@@ -0,0 +1,9 @@
+package identifyHashCode;
+
+public class New_package_at_the_package {
+
+	public static void main(String[] args) {
+
+	}
+
+}
